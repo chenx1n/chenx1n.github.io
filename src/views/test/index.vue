09214e63c60af4666d1f4165af4ec7b27adde34d
ChenXin <39519257+chenxin93@users.noreply.github.com>
2022-08-29 15:50:37 +0800

    二维码捕获

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <select v-model="selected">
+      <option v-for="option in options" :key="option.text" :value="option">
+        {{ option.text }}
+      </option>
+    </select>
     <qrcode-stream
       class="qrcode-stream"
       @decode="onDecode"
@@ -28,7 +33,17 @@ export default {
     // QrcodeCapture,
   },
   data() {
+    const options = [
+      { text: 'nothing (default)', value: undefined },
+      { text: 'outline', value: this.paintOutline },
+      { text: 'centered text', value: this.paintCenterText },
+      { text: 'bounding box', value: this.paintBoundingBox },
+    ];
+    const selected = options[1];
+
     return {
+      selected,
+      options,
       torch: false,
       result: '',
     };
@@ -37,6 +52,23 @@ export default {
     onDecode(url) {
       this.result = url;
     },
+    paintOutline(detectedCodes, ctx) {
+      for (const detectedCode of detectedCodes) {
+        const [firstPoint, ...otherPoints] = detectedCode.cornerPoints;
+
+        ctx.strokeStyle = 'red';
+
+        ctx.beginPath();
+        ctx.moveTo(firstPoint.x, firstPoint.y);
+        for (const { x, y } of otherPoints) {
+          ctx.lineTo(x, y);
+        }
+        ctx.lineTo(firstPoint.x, firstPoint.y);
+        ctx.closePath();
+        ctx.stroke();
+      }
+    },
+
     paintBoundingBox(detectedCodes, ctx) {
       for (const detectedCode of detectedCodes) {
         const {
@@ -46,6 +78,31 @@ export default {
         ctx.lineWidth = 2;
         ctx.strokeStyle = '#007bff';
         ctx.strokeRect(x, y, width, height);
+      }
+    },
+
+    paintCenterText(detectedCodes, ctx) {
+      for (const detectedCode of detectedCodes) {
+        const { boundingBox, rawValue } = detectedCode;
+
+        const centerX = boundingBox.x + boundingBox.width / 2;
+        const centerY = boundingBox.y + boundingBox.height / 2;
+
+        const fontSize = Math.max(
+          12,
+          (50 * boundingBox.width) / ctx.canvas.width
+        );
+        console.log(boundingBox.width, ctx.canvas.width);
+
+        ctx.font = `bold ${fontSize}px sans-serif`;
+        ctx.textAlign = 'center';
+
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#35495e';
+        ctx.strokeText(detectedCode.rawValue, centerX, centerY);
+
+        ctx.fillStyle = '#5cb984';
+        ctx.fillText(rawValue, centerX, centerY);
       }
     },
   },
